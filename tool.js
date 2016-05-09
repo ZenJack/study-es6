b70a6a22a3bf3327a4641b6b504f99e0f9fedf45
ZenJack <zenjack@foxmail.com>
2016-05-09 22:23:11 +0800

    對 tool 工具模塊添加注釋

--- tool.js
+++ tool.js
@@ -1,3 +1,14 @@
+/*
+	一些常用工具方法
+ */
+
+
+/**
+ * 列舉出一個對象中所有可被枚舉的所有屬性
+ * 
+ * @param o 目標對象
+ * @return 目標對象中可以被枚舉的屬性，以數組形式返回 
+ */
 module.exports.enumPro = function(o) {
 	let properties = [];
 	for (p in o) {
@@ -6,6 +17,11 @@ module.exports.enumPro = function(o) {
 	return properties;
 }
 
+/**
+ * 以個性化方法打印出目標對象中可被列舉的屬性
+ * 
+ * @param o 目標對象
+ */
 module.exports.enumProPnt = function(o) {
 	let properties = this.enumPro(o);
 	if (properties.length == 0) return console.log('對象沒有可枚舉的屬性');
