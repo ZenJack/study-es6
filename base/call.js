8983a789ce0c22f252dba392050d8f05de166cb0
ZenJack <zenjack@foxmail.com>
2016-05-10 22:23:21 +0800

    關於 call 和 aplly 的用法

---
+++ base/call.js
@@ -0,0 +1,54 @@
+
+/*
+ * 對象擁有方法，那麼可以已知對象引用，而調用方法，
+ * 這是十分正常的路徑。
+ * 然，有時知道某一方法，想在使用時再確定其所屬對象，
+ * 那麼就要用到 call 或 apply
+ *
+ * 這是一種逆向思維。
+ * 有時這種反向綁定調用會起到十分便捷的效果。
+ */
+
+let o = {
+	name: 'jack',
+	age: 18
+}
+
+function show(caller) {
+	console.log(arguments);
+	console.log(caller + '說:' + this.name + ',' + this.age);
+}
+
+/*
+ * o 為函數 show 的母對象，
+ * 此時在 show 函數內，可以通過 this 來引用 o 對象。
+ *
+ * 餘下的參數即是 show 函數的形參列表
+ *
+ * call 與 apply 的差別在於參數列表傳入形式不同，
+ * call 允許用戶像直接調用目標函數一樣傳參，主要用於手動。
+ * 而 apply 允許用戶以數組形式傳入參數列表，主要用自動化。
+ * 因為在一個函數內，可以通過 arguments 獲得其參數列表。
+ *  
+ */
+show.call(o, 'A');
+
+
+function User(name, age) {
+	this.name = name;
+	this.age = age;
+}
+
+let user = new User('jackson', 20);
+show.call(user, 'B');
+
+function foo(caller) {
+	/*
+	 * arguments 獲取的是類數組對象
+	 *
+	 * apply 參數以數組或類數組的形式
+	 */
+	show.apply(user, arguments);
+}
+
+foo('CY');
