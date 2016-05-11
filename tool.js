744d2c4ef1be63e6404e62e6d17faa67986d5a7b
ZenJack <zenjack@foxmail.com>
2016-05-11 21:00:57 +0800

    在 tool 中添加了類數組轉數組的方法

--- tool.js
+++ tool.js
@@ -45,3 +45,17 @@ module.exports.isLikeArray = function(o) {
 		return false;
 	}
 }
+
+/*
+ * 將一個類數組轉換成數組
+ * 
+ * 類數組說明：
+ * 1. 對象中必有 length 屬性，且其值為正整數，小于 2 的 32 次方
+ * 2. 對象應該有數字屬性（字符數字亦可）
+ * 
+ * @param o 類數組對象
+ * @return 將類數組轉換成數組對象
+ */
+module.exports.toArray = function(o) {
+	return Array.prototype.slice.call(o);
+}
