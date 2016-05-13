c7e33d993220c0286a5ee830cc0f7d5ac8545026
ZenJack <zenjack@foxmail.com>
2016-05-13 22:35:57 +0800

    增加 Set 的部分說明 Signed-off-by: ZenJack <zenjack@foxmail.com>

--- destructuring/array.js
+++ destructuring/array.js
@@ -20,15 +20,32 @@
 // 數組的解構賦值
 
 let [a,b,c] = [1,2,3];
-console.log([a,b,c][0]);	// 1
-console.log([c,a]);			// [ 3, 1 ]
+console.log([a,b,c][0]);	//=> 1
+console.log([c,a]);			//=> [ 3, 1 ]
+
+let arr = [a,b,c];			// 將解構完畢的對象賦值給一個變量
+console.log(arr[0]);		//=> 1
+// 直接訪問解構的變量，雖未像傳統的 let(var,const) 聲明，但它確實可用
+console.log(a);				//=> 1
 
 
-// 對於 Set 的解構
-let [x, y, z] = new Set(['a', 'b', 'c'])
-console.log(x);		// 'a'
+// 對於 Set 的解構， Set 支持數組初始化
+let [x, y, z] = new Set(['a', 'b', 'c']);
+console.log(x);		//=> a
 
-// [a,b,c] 看起來像是一個變量
-for (let i = 0; i < [a,b,c].length; i++) {
-	console.log([a,b,c][i]);
-}
+let set = new Set(['a1', 'b1', 'c1']);
+[x, y, z] = set;
+console.log(x);		//=> a1
+
+set = new Set();
+/*
+ * 1. Set 的 add 函數支持鏈式
+ * 2. 存儲順序為插入順序
+ * 3. 使用 === 判斷元素是否相等
+ * 4. 重復元素直接忽略，以之前的元素為準
+ * 5. 在後續版本中 -0 === 0 為 true
+ */
+set.add('jack').add('bruce').add('jack').add('lisa').add(-0).add(+0);
+[x, y, z] = set;
+console.log([x, y, z]);
+console.log({x, y, z});
